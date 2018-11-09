6381d1b6e1d0a262c389284c4b8d37bd08c85899
zzzzer <zzzzer91@gmail.com>
2018-11-09 13:26:24 +0800

    update

--- 面向对象/09iter.py
+++ 面向对象/09iter.py
@@ -4,6 +4,9 @@
 `__iter__()` 要返回一个实现了 `__next__()` 对象。
 这个对象可以是自身（要实现 `__next__()`），也可以是别的，
 如下面的 `iter(self._list)`。
+
+`Count` 这样的定义也是可以的，因为带有 `yield` 关键字，
+所以 `__iter__()` 本身是个迭代器。
 """
 
 
@@ -13,13 +16,13 @@ class Fib:
 
     def __iter__(self):
         print('__iter__')
-        return self  # 实例本身就是迭代对象，因为实现了 `__next__()`
+        return self  # 实例本身就是迭代器，因为实现了 `__next__()`
 
     def __next__(self):
         print('__next__')
         # 注意这里会改变实例的值，我觉得不太好，可以放到临时变量上
         self.a, self.b = self.b, self.a + self.b  # 计算下一个值
-        if self.a > 100000:  # 退出循环的条件
+        if self.a > 1000:  # 退出循环的条件
             raise StopIteration
         return self.a  # 返回下一个值
 
@@ -30,6 +33,27 @@ class Iter:
 
     def __iter__(self):
         return iter(self._list)
+
+
+class Count:
+    def __init__(self, n):
+        self._n = n
+
+    def __iter__(self):
+        """带 `yield` 关键字，成了生成器（迭代器的一种），
+        不再需要实现 `__next__()`。
+        """
+
+        n = self._n
+        while n > 0:
+            yield n
+            n -= 1
+
+    def __reversed__(self):
+        n = 1
+        while n <= self._n:
+            yield n
+            n += 1
 
 
 def main() -> None:
@@ -44,9 +68,14 @@ def main() -> None:
     # 但是 a 的值也变了
     print(f'>>> {fib.a}')
 
+    print('*'*50)
     it = Iter([1, 2, 3])
     for i in it:
         print(i)
+
+    print('*'*50)
+    for x in Count(5):
+        print(x)
 
 
 if __name__ == '__main__':
